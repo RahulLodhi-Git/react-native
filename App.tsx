499c0aa6b3eb9d2f11016142feb5451e1503e373
Rahul Lodhi <rahul.lodhi5003@gmail.com>
2024-08-23 23:34:41 +0530

    flatcard added with Stylesheet

--- App.tsx
+++ App.tsx
@@ -1,31 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+  ScrollView,
+} from 'react-native';
+import React from 'react';
+import FlatCards from './components/FlatCards';
 
-import React, {useState} from 'react';
-import {SafeAreaView, View, Text, Button} from 'react-native';
-
-function App() {
-  const [greeting, setGreeting] = useState('Hello');
+const App = (): JSX.Element => {
+  const isDarkMode = useColorScheme() === 'dark';
+  console.log('isDarkMode', isDarkMode);
   return (
     <SafeAreaView>
-      <View>
-        <Text style={{fontSize: 40, textAlign: 'center'}}>
-          {greeting} Hi! I'M React Native
-        </Text>
-        <Button
-          title={`Press me and Say Bye`}
-          onPress={() => {
-            setGreeting('Bye');
-          }}
-        />
-      </View>
+      <ScrollView>
+        <FlatCards />
+      </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 export default App;
