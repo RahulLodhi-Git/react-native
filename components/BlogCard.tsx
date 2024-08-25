85b43a775069c6750bbc8e42b44bbaf70308dea5
Rahul Lodhi <rahul.lodhi5003@gmail.com>
2024-08-26 01:32:16 +0530

    blog card images changes

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -17,7 +17,7 @@ const BlogCard = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.headingText}>Blog Card</Text>
-      <ScrollView>
+      <ScrollView horizontal={true}>
         <View style={styles.card}>
           <Image
             src="https://images.unsplash.com/photo-1607799279861-4dd421887fb3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -25,6 +25,63 @@ const BlogCard = () => {
           />
           <Text style={styles.title}>
             Lets Make Blog Card In This Tutorials 1
+          </Text>
+          <Text style={styles.date}>24/Aug/2024</Text>
+          <Text style={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus
+            esse minima eos in ab commodi ipsa! Eligendi, in unde quo accusamus
+            exercitationem maiores, neque vitae repellendus fugit dolorum,
+            cumque illum.
+          </Text>
+          <TouchableOpacity>
+            <Text style={styles.readMore}>Read More</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.card}>
+          <Image
+            src="https://res.cloudinary.com/df7crt9ix/image/upload/v1688295399/pexels_dominika_roseclay_688668_c368f62eef.jpg"
+            style={styles.image}
+          />
+          <Text style={styles.title}>
+            Lets Make Blog Card In This Tutorials 2
+          </Text>
+          <Text style={styles.date}>24/Aug/2024</Text>
+          <Text style={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus
+            esse minima eos in ab commodi ipsa! Eligendi, in unde quo accusamus
+            exercitationem maiores, neque vitae repellendus fugit dolorum,
+            cumque illum.
+          </Text>
+          <TouchableOpacity>
+            <Text style={styles.readMore}>Read More</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.card}>
+          <Image
+            src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            style={styles.image}
+          />
+          <Text style={styles.title}>
+            Lets Make Blog Card In This Tutorials 3
+          </Text>
+          <Text style={styles.date}>24/Aug/2024</Text>
+          <Text style={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus
+            esse minima eos in ab commodi ipsa! Eligendi, in unde quo accusamus
+            exercitationem maiores, neque vitae repellendus fugit dolorum,
+            cumque illum.
+          </Text>
+          <TouchableOpacity>
+            <Text style={styles.readMore}>Read More</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.card}>
+          <Image
+            src="https://plus.unsplash.com/premium_photo-1692241076839-efd08cf9c780?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            style={styles.image}
+          />
+          <Text style={styles.title}>
+            Lets Make Blog Card In This Tutorials 4
           </Text>
           <Text style={styles.date}>24/Aug/2024</Text>
           <Text style={styles.desc}>
@@ -62,9 +119,11 @@ const styles = StyleSheet.create({
     paddingHorizontal: 8,
   },
   card: {
-    backgroundColor: '#ececfa',
+    backgroundColor: '#f1f1f7',
     borderRadius: 5,
     marginBottom: 20,
+    width: deviceWith - 16,
+    marginHorizontal: 8,
   },
   title: {
     fontSize: 25,
